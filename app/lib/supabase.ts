4c775ecd2354759d715a769efa13294672a677f4
Nikesh Rajbhar <nikesh21soni@gmail.com>
2026-05-28 17:37:28 +0530

    fixed build issue

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -12,22 +12,3 @@ export const getAdminClient = () => {
   if (!serviceKey) throw new Error("Admin key is missing (Server-side only)");
   return createClient(supabaseUrl, serviceKey);
 };
-
-
-// import { createClient } from "@supabase/supabase-js";
-
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-// if (!supabaseUrl) {
-//   throw new Error("NEXT_PUBLIC_SUPABASE_URL is missing");
-// }
-
-// if (!supabaseAnonKey) {
-//   throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY is missing");
-// }
-
-// export const supabaseAdmin = createClient(
-//   supabaseUrl,
-//   supabaseAnonKey
-// );
